b4a59a8e6ead1b2fc215b1c21d06ab7c517700b7
Leandro Paniagua <leandrojosepaniaguabalbuena.guadalupe@alumnado.fundacionloyola.net>
2024-03-05 13:43:15 +0100

    05/03/2024 (cambios en el json, index, rest y valtalibro)

---
+++ basededatos.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Autores (
+	idAutor INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	nacionalidad VARCHAR(100) NOT NULL,
+	fecha_nacimiento DATE NOT NULL
+);
+
+-- Crear la tabla de Libros
+CREATE TABLE Libros (
+	idLibro INT AUTO_INCREMENT PRIMARY KEY,
+	titulo VARCHAR(255) NOT NULL,
+	anio_publicacion VARCHAR(4) NOT NULL,
+	isbn VARCHAR NOT NULL,
+	portada BLOB NULL,
+	FOREIGN KEY (idAutor) REFERENCES Autores(idAutor)
+);
+
+
+/*------------------------------------------------------------------------*/
+
+/*{
+    "campos": {
+        "Libros": {
+            "id": "SQLITE3_INTEGER",
+            "titulo": "SQLITE3_TEXT",
+            "anio_publicacion": "SQLITE3_TEXT",
+            "isbn": "SQLITE3_TEXT",
+            "portada": "SQLITE3_BLOB",
+            "id_autor": "SQLITE3_INTEGER"
+        }
+    },
+    "sentencias": {
+        "insertar_obra": "INSERT INTO Libros (titulo, anio_publicacion, isbn) VALUES (:titulo, :anio_publicacion, :isbn);",
+       
+        "actualizar_obra": "UPDATE Libros SET titulo = :nuevoTitulo, anio_publicacion = :nuevoanio_publicacion, portada = :nuevaportada, id_autor = :nuevoid_autor;",
+        "borrar_obra": "DELETE FROM Libros WHERE id IN (:id)",
+        "listar_obra_basico": "SELECT * FROM Libros;"    
+    }
+}*/
