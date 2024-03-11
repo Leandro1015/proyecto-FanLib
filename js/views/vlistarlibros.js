ca4569473cdb57eca3ab6b47f960d822284b0eff
Leandro Paniagua <leandrojosepaniaguabalbuena.guadalupe@alumnado.fundacionloyola.net>
2024-03-11 10:20:39 +0100

    11/03/2024 (voy avanzando)

--- js/views/vlistarlibros.js
+++ js/views/vlistarlibros.js
@@ -10,26 +10,51 @@ export class ListarLibros extends Vista {
 
         this.altaLibro.onclick = this.pulsarAltaLibro.bind(this)
         this.irInicio.onclick = this.pulsarVolverInicio.bind(this)
+    
+        this.restService2 = new Rest()
 
-        this.restService = new Rest()
-
-        this.mostrarLibros
+        this.listarObras()
     }
 
+    async listarObras() {
+        try {
+            const obras = await this.restService2.listarObras() // Llama al método listarObras() del objeto restService2
+            this.mostrarLibros(obras) // Llama al método mostrarLibros() con la lista de obras obtenida
+        } catch (error) {
+            console.error('Error al obtener las obras:', error)
+        }
+    }
+   
     mostrarLibros(libros) {
-        console.log('holA')
-        console.log(libros)
-
-        const listaLibros = document.getElementById('listaLibros')
-        listaLibros.innerHTML = ''
+        console.log('El método mostrarLibros() se está ejecutando.')
     
-        // Iterar sobre la lista de libros y agregar cada uno a la lista
+        const tablaLibros = document.getElementById('tablaLibros')
+    
+        // Limpiar el contenido de la tabla
+        tablaLibros.innerHTML = ''
+    
+        // Iterar sobre la lista de libros y agregar cada uno a la tabla
         libros.forEach(libro => {
-            const itemLibro = document.createElement('li')
-            itemLibro.textContent = `${libro.titulo} - ${libro.anio_publicacion} - ISBN: ${libro.isbn}`
-            listaLibros.appendChild(itemLibro)
+            const fila = document.createElement('tr')
+    
+            // Crear las celdas para cada atributo del libro
+            const columnaTitulo = document.createElement('td')
+            columnaTitulo.textContent = libro.titulo
+            fila.appendChild(columnaTitulo)
+    
+            const columnaAnio = document.createElement('td')
+            columnaAnio.textContent = libro.anio_publicacion
+            fila.appendChild(columnaAnio)
+    
+            const columnaISBN = document.createElement('td')
+            columnaISBN.textContent = libro.isbn
+            fila.appendChild(columnaISBN)
+    
+            // Agregar la fila a la tabla
+            tablaLibros.appendChild(fila)
         })
     }
+    
 
     pulsarAltaLibro() {
         this.controlador.verVista(Vista.valtalibro) // Cambia a la vista para insertar un nuevo libro
